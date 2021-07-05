857f20a2d734daf6bf5e3f668cd4cc54de586ca3
Falat Ekmen <falatekmen@gmail.com>
2021-07-05 13:01:43 +0300

    home screen's API connection completed

--- src/Modules/HomeScreen/HomeScreen.js
+++ src/Modules/HomeScreen/HomeScreen.js
@@ -27,27 +27,25 @@ const HomeScreen = (props) => {
     const _renderEpisodesItem = ({ item }) => {
 
         return (
-            <View style={styles.episodeButton}>
-                <Text style={styles.episodeText}>
-                    {loc.t(texts.season)}{item.episode.slice(1, 3)} - {loc.t(texts.episode)}{item.episode.slice(4, 6)}
-                </Text>
-                <Text style={styles.episodeNameText} numberOfLines={1}>{item.name}</Text>
-            </View>
+            <TouchableOpacity onPress={() => {
+                //navigate edilen ekrana parametre gönderme
+                props.navigation.navigate("episode-detail-screen", {
+                    episodeId: item.id
+                });
+            }}>
+                <View style={styles.episodeButton}>
+                    <Text style={styles.episodeText}>
+                        {loc.t(texts.season)}{item.episode.slice(1, 3)} - {loc.t(texts.episode)}{item.episode.slice(4, 6)}
+                    </Text>
+                    <Text style={styles.episodeNameText} numberOfLines={1}>{item.name}</Text>
+                </View>
+            </TouchableOpacity>
         )
     }
 
-    //başka bir ekrana navigate ederken bu şekilde navigasyon parametreleri gönderebiliyoruz
-    const _onPress_Episode = () => {
-        props.navigation.navigate("episode-detail-screen", {
-            dummy1: 'dummy1',
-            dummy2: 'dummy2'
-        });
-    }
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={_onPress_Episode}>
-                <Text style={{color:'white'}}>Go to Episode Screen</Text>
-            </TouchableOpacity>
+
             <FlatList
                 data={episodeList}
                 renderItem={_renderEpisodesItem}
